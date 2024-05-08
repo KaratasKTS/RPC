09105069ea62304bd33620247b4ecd82b2503398
KaratasKTS <nikoskaratas@gmail.com>
2024-05-08 15:20:05 +0300

    wrote the the 3 functions and the logic, prompt isnt pops up 2 times messing with the program

---
+++ javascript.js
@@ -0,0 +1,55 @@
+//random number for the computer
+function getComputerChoice(){
+    let cchoice = ["rock", "paper", "scissors"];
+    let random = cchoice[Math.floor(cchoice.length*Math.random())];
+    return random;
+}
+//human imput function, returns the human imput in lowercase
+function getHumanChoice(){
+    let hchoice= prompt("rock, paper, scissors");
+    if (hchoice === null) {
+        return null;
+    } 
+    let hchoicelow = hchoice.toLowerCase();
+    if (hchoicelow !== "rock" && hchoicelow !== "paper" && hchoicelow !== "scissors"){
+        return "invalid input";
+    } else{
+    return hchoicelow;
+    }
+}
+
+let humanscore=0;
+let computerscore=0;
+
+/* takes the two previous functions as parameters
+ and runs them throught the if logic */
+
+function playround(humanchoice, computerchoice){
+    if (humanchoice === computerchoice){
+        return "It's a tie!";
+    } else if ((humanchoice === "rock" && computerchoice === "scissors") ||
+               (humanchoice === "paper" && computerchoice === "rock") ||
+               (humanchoice === "scissors" && computerchoice === "paper")){
+        humanscore++; // Human wins
+        return "Human wins!";
+    } else {
+        computerscore++; // Computer wins
+        return "Computer wins!";
+    }
+}
+
+//passing the functions in variables 
+let humanc = getHumanChoice();
+let computerc = getComputerChoice();
+
+
+
+//console log the picks
+console.log("human:"+getHumanChoice());
+console.log("computer:"+getComputerChoice());
+
+
+
+
+// init the functionpassing the variables as arguments
+console.log (playround(humanc,computerc));
