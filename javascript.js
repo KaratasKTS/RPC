ab413a088427854175ec2d84d8b3f933e2e36ab0
KaratasKTS <nikoskaratas@gmail.com>
2024-05-09 13:35:51 +0300

    i looped the program in a while loop making the game go until one variable reaches 5, this program  is done,simple yet harder than i expected :D

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ function getHumanChoice(){
     if (hchoice === null) {
         return null;
     } 
-    let hchoicelow = hchoice.toLowerCase();
+    let hchoicelow = (hchoice.toLowerCase(),hchoice.trim());
     if (hchoicelow !== "rock" && hchoicelow !== "paper" && hchoicelow !== "scissors"){
         return "invalid input";
     } else{
@@ -22,7 +22,7 @@ function getHumanChoice(){
  let humanscore = 0;
  let computerscore = 0;
 
-/* takes the two previous functions as parameters
+/* takes the two previous functions as arguments 
  and runs them throught the if logic */
 function playround(humanchoice, computerchoice){
     if (humanchoice === computerchoice){
@@ -38,22 +38,18 @@ function playround(humanchoice, computerchoice){
     }
 }
 
-
-//passing the functions in variables 
-let humanc = getHumanChoice();
-let computerc = getComputerChoice();
-
-
-// init the functionpassing the variables as arguments
-
-do {
-    console.log(playround(humanc, computerc));
-  } while (humanscore < 5 && computerscore < 5);
-
-//console log scores and picks the picks
-console.log("human:" + humanc);
-console.log("computer:" + computerc);
+while (humanscore < 5 && computerscore < 5 ){
+    let humanchoice = getHumanChoice();
+    let computerchoice = getComputerChoice();
+    console.log(playround(humanchoice, computerchoice));
+    console.log("human:" + humanchoice);
+console.log("computer:" + computerchoice);
 console.log("human score:" + humanscore);
 console.log("computer score:" + computerscore);
+}
+
+
+//console log scores and picks the picks
+
 
 
